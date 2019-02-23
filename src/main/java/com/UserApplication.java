4d640ea89d5cc0e144becd832956fde8c59766f6
ghsi10 <ghsiiscool@walla.com>
2019-02-23 02:39:14 +0200

    RestTemplate to WebClient.

--- src/main/java/com/UserApplication.java
+++ src/main/java/com/UserApplication.java
@@ -8,33 +8,29 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @RestController
 @RibbonClient(name = "hello", configuration = DefaultRibbonConfig.class)
 public class UserApplication {
 
-    private RestTemplate restTemplate;
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
-    @LoadBalanced
     @Bean
-    RestTemplate restTemplate() {
-        return new RestTemplate();
+    @LoadBalanced
+    public WebClient.Builder loadBalancedWebClientBuilder() {
+        return WebClient.builder();
     }
 
     @RequestMapping("/hi")
     public String hi() {
-        String greeting = this.restTemplate.getForObject("http://hello", String.class);
+        String greeting = webClientBuilder.build().get().uri("http://hello").retrieve().bodyToMono(String.class).block();
         return String.format("%s!", greeting);
     }
 
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
-    }
-
-    @Autowired
-    public void setRestTemplate(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
     }
 }
